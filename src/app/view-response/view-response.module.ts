62e8f0a3fac7e9471fc54b6202bda9002df54919
Pavithra <paviprakash222@gmail.com>
2020-07-08 16:51:13 +0530

    5 commit

--- src/app/view-response/view-response.module.ts
+++ src/app/view-response/view-response.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ViewResponseComponent } from './view-response.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      { path: 'viewresponse', component: ViewResponseComponent ,children:[ 
+      
+  ]}
+  ])
   ]
 })
 export class ViewResponseModule { }
